e671c05604e7082f7ba2d5242f58824e5d8c5a8d
Jun <inakademac@gmail.com>
2024-08-23 08:56:59 +0900

    [Refactor]Elementsコンポーネント実装(不要コード除去)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -94,62 +94,6 @@ const App: React.FC = () => {
     updateConnectionInRoutine(selectEvent, routine, setRoutine);
   }, [routine]);
 
-  // 技選択時のhandle関数
-  const handleElementClick = (element: Element) => {
-    if (getElementStatus(routine, element) === ElementStatus.選択済み) {
-      setRoutine(routine.filter((e) => e.id !== element.id));
-      return;
-    }
-    if (getElementStatus(routine, element) === ElementStatus.選択可能) {
-      const newRoutineElement: RoutineElement = {
-        ...element,
-        is_connected: false,
-        element_group_score: 0,
-        connection_value: null,
-      };
-      setRoutine([...routine, newRoutineElement]);
-    }
-  };
-
-  // ElementStatusを表示する関数 TODO: Rulesに統合
-  const renderElementStatusLabel = (element: Element) => {
-    const status = getElementStatus(routine, element);
-    // 選択可能 → 何も表示しない
-    if (status === ElementStatus.選択可能) {
-      return null;
-    }
-    // 選択済み → 選択済み(技番号)
-    if (status === ElementStatus.選択済み) {
-      const index = routine.findIndex((e) => e.id === element.id);
-      return (
-        <div className="common__label common__label--active">{`選択済み(${
-          index + 1
-        }技目)`}</div>
-      );
-    }
-    // 同一枠選択済み → 同一枠選択済み(技番号)
-    if (status === ElementStatus.同一枠選択済み) {
-      const code = routine.find((e) => e.code === element.code)?.code;
-      return <div className="common__label">{`同一枠(${code})`}</div>;
-    }
-    // 技数制限_グループ → 技数制限_グループ
-    if (status === ElementStatus.技数制限_グループ) {
-      return <div className="common__label">グループ技数制限</div>;
-    }
-    // 技数制限_全体 → 技数制限_全体
-    if (status === ElementStatus.技数制限_全体) {
-      return <div className="common__label">全体技数制限</div>;
-    }
-    // 床_力技制限
-    if (status === ElementStatus.床_力技制限) {
-      return <div className="common__label">力技制限</div>;
-    }
-    // 床_旋回制限
-    if (status === ElementStatus.床_旋回制限) {
-      return <div className="common__label">旋回制限</div>;
-    }
-  };
-
   // そもそも組み合わせさせないための処理
   const handleConnectionClick = (element: RoutineElement, index: number) => {
     // 更新用関数
